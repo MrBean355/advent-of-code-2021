146650464cda8e170bec5346e479d7f75cbf8512
Mike Johnston <mrbean355@gmail.com>
2021-12-14 19:40:54 +0200

    Implement day 14 part 2 solution

--- src/main/kotlin/com/github/mrbean355/aoc/day14/Day14.kt
+++ src/main/kotlin/com/github/mrbean355/aoc/day14/Day14.kt
@@ -5,30 +5,52 @@ import com.github.mrbean355.aoc.base.Puzzle
 class Day14(private val input: List<String>) : Puzzle {
 
     override fun part1(): Number {
-        val polymer = loadPolymer().toMutableList()
-        val rules = loadRules()
-
-        repeat(10) {
-            var i = 0
-            while (i < polymer.size - 1) {
-                val pair = polymer[i] + polymer[i + 1].toString()
-                polymer.add(i + 1, rules.getValue(pair))
-                i += 2
-            }
-        }
-
-        val counts = polymer.distinct().map { ch ->
-            polymer.count { it == ch }
-        }
-
-        return counts.maxOf { it } - counts.minOf { it }
+        return polymerize(iterations = 10)
     }
 
     override fun part2(): Number {
-        TODO("Not yet implemented")
+        return polymerize(iterations = 40)
     }
 
-    private fun loadPolymer(): String {
+    private fun polymerize(iterations: Int): Long {
+        val template = loadPolymerTemplate()
+        val rules = loadRules()
+
+        val uniqueLetters = rules
+            .flatMap { (pair, insertion) -> listOf(pair[0], pair[1], insertion) }
+            .distinct()
+
+        val pairCounts = uniqueLetters
+            .flatMap { letter -> uniqueLetters.map { other -> "$letter$other" } }
+            .associateWith { pair -> template.split(pair).size.toLong() - 1 }
+            .toMutableMap()
+
+        repeat(iterations) {
+            pairCounts.filterValues { it > 0 }.forEach { (pair, count) ->
+                val insertion = rules.getValue(pair)
+                val firstNewPair = pair[0].toString() + insertion
+                val secondNewPair = insertion.toString() + pair[1]
+
+                pairCounts.add(pair, -count)
+                pairCounts.add(firstNewPair, count)
+                pairCounts.add(secondNewPair, count)
+            }
+        }
+
+        val letterCounts = uniqueLetters.associateWith { letter ->
+            pairCounts.mapValues { (pair, count) ->
+                (pair.count { it == letter } * count)
+            }.values.sum().inc() / 2
+        }.values
+
+        return letterCounts.maxOf { it } - letterCounts.minOf { it }
+    }
+
+    private fun MutableMap<String, Long>.add(key: String, amount: Long) {
+        put(key, getValue(key) + amount)
+    }
+
+    private fun loadPolymerTemplate(): String {
         return input.first()
     }
 
--- src/test/kotlin/com/github/mrbean355/aoc/day14/Day14Test.kt
+++ src/test/kotlin/com/github/mrbean355/aoc/day14/Day14Test.kt
@@ -5,11 +5,12 @@ import com.github.mrbean355.aoc.base.PuzzleTest
 class Day14Test : PuzzleTest(Day14::class) {
 
     override val part1TestCases = mapOf(
-        "day14/example.txt" to 1588,
-        "day14/puzzle.txt" to 3213,
+        "day14/example.txt" to 1588L,
+        "day14/puzzle.txt" to 3213L,
     )
 
-    override val part2TestCases: Map<String, Any>
-        get() = emptyMap()
-
+    override val part2TestCases = mapOf(
+        "day14/example.txt" to 2188189693529L,
+        "day14/puzzle.txt" to 3711743744429L,
+    )
 }
